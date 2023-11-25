c471a8a415b29ea06ec210abc09f3f01ecf3e35d
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-11-25 11:47:38 +0600

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ link.addEventListener('click', () => {
 // Perform desired action when the link is clicked
 });
 
+
+
+var menuBtn = document.getElementById("menuBtn")
+var sideNav = document.getElementById("sideNav")
+var menu = document.getElementById("menu")
